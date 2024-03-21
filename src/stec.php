0850a52149097f75341a0b4b1293213515d783f0
stachethemes <stachethemes@gmail.com>
2024-03-21 18:36:17 +0200

    5.2.7 ver string

--- src/stec.php
+++ src/stec.php
@@ -10,7 +10,7 @@ define('STEC_LITE_PLUGIN_FILE', __FILE__);
 
 class Stachethemes_Event_Calendar {
 
-    public $version = '5.2.4';
+    public $version = '5.2.7';
 
     private static $instance;
 
