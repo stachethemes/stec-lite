1dce3465a8df0f9bde6ec988440f8392d6107f07
stachethemes <stachethemes@gmail.com>
2024-03-26 13:48:18 +0200

    removed unused lib

--- src/js/common-components/Highlight.js
+++ src/js/common-components/Highlight.js
@@ -1,57 +1,11 @@
-import { getPluginUrl } from '@Stec/JS/helpers';
-import { useEffect, useRef } from 'react';
-
 function Highlight(props) {
 
-    const container = useRef(null);
-    const propsDep = JSON.stringify(props);
-
-    useEffect(() => {
-
-        let mounted = true;
-
-        const initHlJs = () => {
-
-            if (mounted) {
-
-                window.hljs.configure({
-                    // react by default escapes html
-                    // hljs triggers false positive ?
-                    ignoreUnescapedHTML: true 
-                });
-
-                window.hljs.highlightElement(container.current);
-
-                container.current.parentElement.style.display = 'block'
-
-            }
-
-        }
-
-        if (window.hljs) {
-            initHlJs();
-            return;
-        }
-
-        const script = document.createElement('script');
-        script.src = getPluginUrl('assets/js/libs/highlight.min.js');
-        script.async = true;
-        script.onload = () => {
-            initHlJs();
-        };
-
-        document.body.appendChild(script);
-
-        return () => {
-            mounted = false;
-            document.body.removeChild(script);
-        }
-
-    }, [propsDep]);
+    // Highlight component is removed for lite version
+    // This component is here to preserve the code structure
 
     return (
-        <pre className='stec-highlight' style={{ display: 'none' }}>
-            <code className={`language-${props.lang}`} ref={container}>
+        <pre className='stec-highlight'>
+            <code className={`language-${props.lang}`}>
                 {props.children}
             </code>
         </pre>
