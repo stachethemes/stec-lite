5582d1db75e68c2d125ad6e5119cbefbfa3d6e55
stachethemes <stachethemes@gmail.com>
2024-03-26 14:12:42 +0200

    do not use variables or function calls for the text

--- src/includes/posts/trait.post-helpers.php
+++ src/includes/posts/trait.post-helpers.php
@@ -11,36 +11,36 @@ trait Post_Trait_Helpers {
     public static function get_post_labels($single, $plural) {
 
         return array(
-            'name'                       => esc_html_x($plural, 'post general name', 'stachethemes_event_calendar_lite'),
-            'singular_name'              => esc_html_x($single, 'post singular name', 'stachethemes_event_calendar_lite'),
-            'add_new'                    => esc_html_x('Add New', 'event', 'stachethemes_event_calendar_lite'),
-            'add_new_item'               => esc_html_x(sprintf('Add new %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'edit_item'                  => esc_html_x(sprintf('Edit %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'new_item'                   => esc_html_x(sprintf('New %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'view_item'                  => esc_html_x(sprintf('View %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'view_items'                 => esc_html_x(sprintf('New %s', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
-            'search_items'               => esc_html_x(sprintf('Search %s', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
-            'not_found'                  => esc_html_x(sprintf('No %s found', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
-            'not_found_in_trash'         => esc_html_x(sprintf('No %s found in trash', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'name'                       => esc_html($plural),
+            'singular_name'              => esc_html($single, 'post singular name', 'stachethemes_event_calendar_lite'),
+            'add_new'                    => esc_html__('Add New', 'stachethemes_event_calendar_lite'),
+            'add_new_item'               => sprintf(esc_html_x('Add New %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'edit_item'                  => sprintf(esc_html_x('Edit %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'new_item'                   => sprintf(esc_html_x('New %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'view_item'                  => sprintf(esc_html_x('View %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'view_items'                 => sprintf(esc_html_x('View %s', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'search_items'               => sprintf(esc_html_x('Search %s', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'not_found'                  => sprintf(esc_html_x('No %s found', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'not_found_in_trash'         => sprintf(esc_html_x('No %s found in trash', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
             'parent_item_colon'          => esc_html__('Parent Page:', 'stachethemes_event_calendar_lite'),
-            'all_items'                  => esc_html_x(sprintf('All %s', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
-            'archives'                   => esc_html_x(sprintf('%s archive', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'attributes'                 => esc_html_x(sprintf('%s attributes', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'insert_into_item'           => esc_html_x(sprintf('Insert into %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'uploaded_to_this_item'      => esc_html_x(sprintf('Uploaded to this %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'featured_image'             => esc_html_x('Featured image', 'event', 'stachethemes_event_calendar_lite'),
-            'set_featured_image'         => esc_html_x('Set featured image', 'event', 'stachethemes_event_calendar_lite'),
-            'remove_featured_image'      => esc_html_x('Remove featured image', 'event', 'stachethemes_event_calendar_lite'),
-            'use_featured_image'         => esc_html_x('Use as featured image', 'event', 'stachethemes_event_calendar_lite'),
-            'filter_items_list'          => esc_html_x(sprintf('Filter %s list', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'all_items'                  => sprintf(esc_html_x('All %s', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'archives'                   => sprintf(esc_html_x('%s archive', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'attributes'                 => sprintf(esc_html_x('%s attributes', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'insert_into_item'           => sprintf(esc_html_x('Insert into %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'uploaded_to_this_item'      => sprintf(esc_html_x('Uploaded to this %s', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'featured_image'             => esc_html__('Featured image', 'stachethemes_event_calendar_lite'),
+            'set_featured_image'         => esc_html__('Set featured image', 'stachethemes_event_calendar_lite'),
+            'remove_featured_image'      => esc_html__('Remove featured image', 'stachethemes_event_calendar_lite'),
+            'use_featured_image'         => esc_html__('Use as featured image', 'stachethemes_event_calendar_lite'),
+            'filter_items_list'          => sprintf(esc_html_x('Filter %s list', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
             'filter_by_date'             => esc_html__('Filter by date', 'stachethemes_event_calendar_lite'),
-            'items_list_navigation'      => esc_html_x(sprintf('%s list navigation', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
-            'items_list'                 => esc_html_x(sprintf('%s list', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
-            'item_published'             => esc_html_x(sprintf('%s published', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'item_published_privately'   => esc_html_x(sprintf('%s published privately', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'item_reverted_to_draft'     => esc_html_x(sprintf('%s reverted to draft', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'item_scheduled'             => esc_html_x(sprintf('%s scheduled', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
-            'item_updated'               => esc_html_x(sprintf('%s scheduled', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'items_list_navigation'      => sprintf(esc_html_x('%s list navigation', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'items_list'                 => sprintf(esc_html_x('%s list', 'post plural name', 'stachethemes_event_calendar_lite'), $plural),
+            'item_published'             => sprintf(esc_html_x('%s published', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'item_published_privately'   => sprintf(esc_html_x('%s published privately', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'item_reverted_to_draft'     => sprintf(esc_html_x('%s reverted to draft', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'item_scheduled'             => sprintf(esc_html_x('%s scheduled', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
+            'item_updated'               => sprintf(esc_html_x('%s updated', 'post singular name', 'stachethemes_event_calendar_lite'), $single),
         );
     }
 
@@ -51,7 +51,7 @@ trait Post_Trait_Helpers {
          * READ / EDIT / DELETE REST routes should check 
          * if these conditions are met first
          */
-        
+
         return array(
             'edit_post'          => 'manage_' . $post_type,
             'edit_posts'         => 'manage_' . $post_type,
