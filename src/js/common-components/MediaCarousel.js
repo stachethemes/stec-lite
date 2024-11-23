98f4015eff8703571546132b79bb0380b48fc562
zbozhilov <zbozhilov@gmail.com>
2024-11-23 14:07:16 +0200

    Refactor media carousel styles for improved responsiveness and animation effects

--- src/js/common-components/MediaCarousel.js
+++ src/js/common-components/MediaCarousel.js
@@ -1,230 +1,124 @@
-import { useImagesLoaded } from '@Stec/JS/calendar/hooks';
-import { isMobile } from '@Stec/JS/helpers';
-import { StecDiv } from '@Stec/WebComponents';
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Navigation, Thumbs } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import ModalMediaCarousel from './ModalMediaCarousel';
+import { StecDiv } from '@Stec/WebComponents';
 
-const Thumb = ({ src, active, onClick }) => {
+const ImageItem = ({ src }) => {
 
-    const classNameArray = ['stec-media-carousel-thumbnail'];
+    const [loaded, setLoaded] = useState(false);
 
-    if (active) {
-        classNameArray.push('active');
-    }
-
-    return <StecDiv
-        className={classNameArray.join(' ')}
-        style={{ backgroundImage: `url(${src})` }}
-        onClick={onClick}></StecDiv>
-}
-
-const Image = ({ src, active, onClick }) => {
-
-    const classNameArray = ['stec-media-carousel-image'];
-
-    if (active) {
-        classNameArray.push('active');
-    }
-
-    return <StecDiv
-        className={classNameArray.join(' ')}
-        onClick={onClick}
-        style={{ backgroundImage: `url(${src})` }}></StecDiv>
-
-}
-
-const MediaCarouselContent = ({ images }) => {
-
-    const [active, setActive] = useState(images[0].id);
-    const [modalOpen, setModalOpen] = useState(false);
-
-    const imagesContainerRef = useRef();
-    const showThumbs = images.length > 1;
-
-    // Function to mouse move image position if images don't comform the aspect ratio
-    useEffect(() => {
-        const activeElement = imagesContainerRef.current.getElementsByClassName('active')[0];
-
-        const imageMouseOut = () => {
-            activeElement.style.backgroundPositionX = '50%';
-            activeElement.style.backgroundPositionY = '50%';
-            activeElement.style.transition = 'opacity .5s linear, background-position .5s ease';
-        };
-
-        const imageMouseMove = (e) => {
-            const x = e.offsetX;
-            const y = e.offsetY;
-            const w = activeElement.offsetWidth;
-            const h = activeElement.offsetHeight;
-
-            const xPos = Math.max(0, Math.round((x / w) * 100));
-            const yPos = Math.max(0, Math.round((y / h) * 100));
-
-            activeElement.style.transition = 'none';
-            activeElement.style.backgroundPosition = `${xPos}% ${yPos}%`;
-        };
-
-        activeElement.addEventListener('mousemove', imageMouseMove, { passive: true });
-        activeElement.addEventListener('mouseout', imageMouseOut, { passive: true });
-
-        return () => {
-            activeElement.removeEventListener('mousemove', imageMouseMove);
-            activeElement.removeEventListener('mouseout', imageMouseOut);
-        };
-
-    }, [active]);
-
-    // Function to slide images on mobile
     useEffect(() => {
 
-        if (!isMobile || !imagesContainerRef.current) {
-            return;
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+            setLoaded(true);
         }
 
-        let touchStartX = 0;
+    }, [src, loaded]);
 
-        const slidePrev = () => {
-
-            const prevId = images[images.findIndex(image => image.id === active) - 1]?.id;
-
-            if (prevId) {
-                setActive(prevId);
-            }
-   
-        }
-
-        const slideNext = () => {
-
-            const nextId = images[images.findIndex(image => image.id === active) + 1]?.id;
-
-            if (nextId) {
-                setActive(nextId);
-            }
-     
-        }
-
-        const handleTouchStart = (event) => {
-            touchStartX = event.touches[0]?.clientX || 0;
-        };
-
-        const handleTouchEnd = (event) => {
-            const touchEndX = event.changedTouches[0]?.clientX || 0;
-            const deltaX = touchEndX - touchStartX;
-            const threshold = 50;
-
-            if (deltaX > threshold) {
-                slidePrev();
-            } else if (deltaX < -threshold) {
-                slideNext();
-            }
-        };
-
-        const element = imagesContainerRef.current;
-
-        element.addEventListener('touchstart', handleTouchStart);
-        element.addEventListener('touchend', handleTouchEnd);
-
-        return () => {
-            element.removeEventListener('touchstart', handleTouchStart);
-            element.removeEventListener('touchend', handleTouchEnd);
-        };
-
-    }, [images, active]);
+    if (!loaded) {
+        return <StecDiv className='stec-media-carousel-placeholder'></StecDiv>
+    }
 
     return (
-        <>
-            {
-                <ModalMediaCarousel
-                    active={active}
-                    onChange={value => {
-                        setActive(value);
-                    }}
-                    images={images}
-                    isOpen={modalOpen}
-                    onClose={() => {
-                        setModalOpen(false)
-                    }}
-                />
-            }
-
-            {
-                showThumbs &&
-                <StecDiv className='stec-media-carousel-thumbnails'>
-                    {
-                        images.map(image => {
-
-                            const isActive = image.id === active;
-
-                            return (
-                                <Thumb key={image.id} src={image.sizes.thumbnail} active={isActive} onClick={() => {
-                                    setActive(image.id);
-                                }} />
-                            )
-                        })
-                    }
-                </StecDiv>
-            }
-
-            <StecDiv className='stec-media-carousel-images' ref={imagesContainerRef}>
-                {
-                    images.map(image => {
-
-                        const isActive = image.id === active;
-
-                        return (
-                            <Image key={image.id} src={image.sizes.full} active={isActive} onClick={() => {
-                                setModalOpen(true);
-                            }} />
-                        )
-                    })
-                }
-            </StecDiv>
-
-        </>
+        <StecDiv className='stec-media-carousel-image'><img src={src} alt='media' /></StecDiv>
     )
 
 }
 
-const MediaCarousel = ({ images, style }) => {
+const MediaCarousel = ({ images, classes, style }) => {
+
+    const [active, setActive] = useState(false);
+
+    const classNameArray = ['stec-media-carousel'];
+
+    if (classes) {
+        classNameArray.push(classes);
+    }
+
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
     const uniqueImages = [...new Map(images.map(item =>
         [item['id'], item])).values()];
 
-    const { ready: imagesLoaded } = useImagesLoaded(uniqueImages.map(image => image.sizes.full));
 
-    const classNameArray = ['stec-media-carousel'];
+    const hasThumbnails = uniqueImages.length > 1;
 
-    const showThumbs = uniqueImages.length > 1;
+    const getThumbnailsPerView = (imagesLength) => {
 
-    const hasDimensionsData = images[0].dimensions ? true : false;
+        if (typeof window.stecFilterMediaCarouselThumbsPerView === 'function') {
+            return window.stecFilterMediaCarouselThumbsPerView(imagesLength);
+        }
 
-    const paddingTop = images.length === 1 && hasDimensionsData ?
-        `calc(100% / ${images[0].dimensions.full.ar} )` :
-        `calc(100% / var(--stec-media-carousel-ratio) )`;
-
-    const withPaddingTopStyle = {
-        ...style,
-        paddingTop: paddingTop
+        return imagesLength > 4 ? 4 : imagesLength;
     }
 
-    if (imagesLoaded) {
-        classNameArray.push('stec-media-carousel-images-ready');
-    }
+    const thumbsPerView = getThumbnailsPerView(uniqueImages.length);
 
-    if (!showThumbs) {
-        classNameArray.push('no-thumbs');
-    }
+    const swipeSpeed = window.stecFilterMediaCarouselSpeed ?? 500;
 
     return (
-        <StecDiv
-            className={classNameArray.join(' ')}
-            style={withPaddingTopStyle}>
+        <>
+            <ModalMediaCarousel
+                active={active}
+                images={images}
+                isOpen={!!active}
+                onClose={() => {
+                    setActive(false)
+                }}
+            />
 
-            {
-                imagesLoaded && <MediaCarouselContent images={uniqueImages} />
-            }
+            <StecDiv className={classNameArray.join(' ')} style={style}>
 
-        </StecDiv>
+                {/* Main swiper */}
+                <Swiper
+                    speed={swipeSpeed}
+                    className='stec-media-carousel-main'
+                    spaceBetween={10}
+                    slidesPerView={1}
+                    navigation={true}
+                    loop={uniqueImages.length > 1}
+                    modules={[Thumbs, Navigation]}
+                    thumbs={{ swiper: thumbsSwiper }}>
+                    {uniqueImages.map(item => {
+                        return (
+                            <SwiperSlide key={item.id} onClick={() => {
+                                setActive(item.id);
+                            }}>
+                                <ImageItem src={item.sizes.full} />
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
+
+                {/* Thumbs Swiper */}
+                {hasThumbnails && <StecDiv className='stec-media-carousel-thumbs-wrapper'>
+                    <Swiper
+                        className='stec-media-carousel-thumbs'
+                        modules={[Thumbs]}
+                        watchSlidesProgress
+                        spaceBetween={10}
+                        slidesPerView={thumbsPerView}
+                        onSwiper={setThumbsSwiper}
+                    >
+                        {uniqueImages.map(item => {
+                            return (
+                                <SwiperSlide key={item.id}>
+                                    <ImageItem src={item.sizes.large} />
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                </StecDiv>
+                }
+            </StecDiv>
+
+        </>
+
     )
 
 }
--- src/js/common-components/ModalMediaCarousel.js
+++ src/js/common-components/ModalMediaCarousel.js
@@ -1,247 +1,61 @@
 import Modal from '@Stec/CommonComponents/Modal';
-import { useImagesLoaded } from '@Stec/JS/calendar/hooks';
-import { isMobile } from '@Stec/JS/helpers';
 import { StecDiv } from '@Stec/WebComponents';
-import { useEffect, useRef, useState } from 'react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/zoom';
 
-const Thumb = ({ src, active, onClick }) => {
-
-    const classNameArray = ['stec-media-carousel-modal-thumbnail'];
-
-    if (active) {
-        classNameArray.push('active');
-    }
-
-    return <StecDiv
-        className={classNameArray.join(' ')}
-        style={{ backgroundImage: `url(${src})` }}
-        onClick={onClick}></StecDiv>
-
-}
-
-const Image = ({ src, active, onClick }) => {
-
-    const classNameArray = ['stec-media-carousel-modal-image'];
-
-    const imageRef = useRef();
-
-    if (active) {
-        classNameArray.push('active');
-    }
-
-    useEffect(() => {
-
-        if (active) {
-            imageRef.current.focus();
-        }
-
-    }, [active])
-
-    return <img
-        onClick={onClick}
-        tabIndex='-1'
-        style={{ outline: 'none' }}
-        ref={imageRef}
-        className={classNameArray.join(' ')}
-        src={src} />
-
-}
+import { Keyboard, Mousewheel, Navigation, Zoom, Scrollbar } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const ModalMediaCarousel = ({ images, active: initActive, isOpen, onClose, onChange }) => {
 
     const uniqueImages = [...new Map(images.map(item => [item['id'], item])).values()];
 
-    const [active, setActive] = useState(false);
-    
-    const { ready: imageLoaded } = useImagesLoaded(active ? [
-        uniqueImages.find(image => image.id === active)?.sizes?.full
-    ] : []);
-    const [showThumbs, setShowThumbs] = useState(true);
-    const uniqueImagesDep = JSON.stringify(uniqueImages);
-    const imagesContainerRef = useRef();
+    const kbdSettings = { enabled: true, onlyInViewport: true, };
+    const arrowsEnabled = true;
+    const swipeSpeed = window.stecFilterCarouselSpeed ?? 450;
+    const scrollBarSettings = { hide: true }
+    const initialIndex = uniqueImages.findIndex((image) => image.id === initActive);
 
-    useEffect(() => {
-
-        setActive(initActive);
-
-    }, [initActive]);
-
-    useEffect(() => {
-
-        if (false === imageLoaded) {
-            return;
-        }
-
-        function bindKeyboard(e) {
-
-            switch (e.which) {
-
-                // case escape key
-                case 27: {
-                    onClose();
-                    break;
-                }
-
-                // case left key
-                case 37: {
-
-                    let imageIndex = uniqueImages.findIndex(test => test.id === active);
-
-                    if (imageIndex <= 0) {
-                        imageIndex = uniqueImages.length - 1;
-                    } else {
-                        imageIndex = imageIndex - 1;
-                    }
-
-                    let nextImage = uniqueImages[imageIndex].id;
-
-                    setActive(nextImage);
-
-                    break;
-                }
-
-                // case right key
-                case 39: {
-
-                    let imageIndex = uniqueImages.findIndex(test => test.id === active);
-
-                    if (imageIndex + 1 >= uniqueImages.length) {
-                        imageIndex = 0;
-                    } else {
-                        imageIndex = imageIndex + 1;
-                    }
-
-                    let nextImage = uniqueImages[imageIndex].id;
-
-                    setActive(nextImage);
-
-                    break;
-                }
-            }
-        }
-
-        document.addEventListener('keydown', bindKeyboard);
-
-        return () => {
-            document.removeEventListener('keydown', bindKeyboard);
-        }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [active, uniqueImagesDep, imageLoaded]);
-
-    // Function to slide images on mobile
-    useEffect(() => {
-
-        if (!isMobile || !imagesContainerRef.current || !isOpen) {
-            return;
-        }
-
-        let touchStartX = 0;
-
-        const slidePrev = () => {
-
-            const prevId = images[images.findIndex(image => image.id === active) - 1]?.id;
-
-            if (prevId) {
-                setActive(prevId);
-            }
-
-        }
-
-        const slideNext = () => {
-
-            const nextId = images[images.findIndex(image => image.id === active) + 1]?.id;
-
-            if (nextId) {
-                setActive(nextId);
-            }
-
-        }
-
-        const handleTouchStart = (event) => {
-            touchStartX = event.touches[0]?.clientX || 0;
-        };
-
-        const handleTouchEnd = (event) => {
-            const touchEndX = event.changedTouches[0]?.clientX || 0;
-            const deltaX = touchEndX - touchStartX;
-            const threshold = 50;
-
-            if (deltaX > threshold) {
-                slidePrev();
-            } else if (deltaX < -threshold) {
-                slideNext();
-            }
-        };
-
-        const element = imagesContainerRef.current;
-
-        element.addEventListener('touchstart', handleTouchStart);
-        element.addEventListener('touchend', handleTouchEnd);
-
-        return () => {
-            element.removeEventListener('touchstart', handleTouchStart);
-            element.removeEventListener('touchend', handleTouchEnd);
-        };
-
-    }, [images, active, isOpen]);
-
-    if (false === active) {
-        return null;
-    }
 
     return (
         <Modal
             isOpen={isOpen}
             onClose={onClose}
             maxWidth='100%' plain={true} overlayColor={'rgba(0,0,0,0.95)'}>
+            <StecDiv className='stec-media-carousel-modal'>
+                <Swiper
+                    zoom={true}
+                    centeredSlides={true}
+                    speed={swipeSpeed}
+                    direction={'horizontal'}
+                    mousewheel={true}
+                    loop={uniqueImages.length > 1}
+                    grabCursor={true}
+                    slidesPerView={1}
+                    keyboard={kbdSettings}
+                    modules={[Keyboard, Mousewheel, Navigation, Zoom, Scrollbar]}
+                    initialSlide={initialIndex}
+                    navigation={arrowsEnabled}
+                    scrollbar={scrollBarSettings}
+                    spaceBetween={10}
+                >
+                    {
+                        uniqueImages.map((image) => {
 
-            <StecDiv className='stec-media-carousel-modal' style={{ height: 'auto' }} onClick={onClose}>
-
-                {
-                    (showThumbs && uniqueImages.length > 1) && <StecDiv className='stec-media-carousel-modal-thumbnails'>
-                        {
-                            uniqueImages.map(image => {
-
-                                const isActive = image.id === active;
-
-                                return (
-                                    <Thumb key={image.id} src={image.sizes.thumbnail} active={isActive} onClick={(e) => {
-                                        e.stopPropagation();
-                                        setActive(image.id);
-                                        if (onChange) {
-                                            onChange(image.id);
-                                        }
-                                    }} />
-                                )
-                            })
-                        }
-                    </StecDiv>
-                }
-
-                {
-                    imageLoaded && <StecDiv
-                        ref={imagesContainerRef}
-                        onClick={onClose}
-                        className={`stec-media-carousel-modal-images ${!showThumbs ? 'no-thumbs' : ''}`}>
-                        {
-                            uniqueImages.map(image => {
-
-                                const isActive = image.id === active;
-
-                                return (
-                                    <Image key={image.id} src={image.sizes.full} active={isActive} onClick={(e) => {
-                                        e.stopPropagation();
-                                        setShowThumbs(state => !state);
-                                    }} />
-                                )
-                            })
-                        }
-                    </StecDiv>
-                }
-
+                            return (
+                                <SwiperSlide key={image.id} >
+                                    <div className='swiper-zoom-container'>
+                                        <img src={image.sizes.full} />
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+                </Swiper>
             </StecDiv>
-
         </Modal>
     )
 }
